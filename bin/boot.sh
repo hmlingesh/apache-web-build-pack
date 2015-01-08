df4b3d0235076d7430e4d511b555b6a63f8fd4f1
hmlingesh <Lingesh.M@happiestminds.com>
2015-01-08 20:33:10 +0530

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -23,6 +23,6 @@ cat $APP_ROOT/apache2/conf/httpd.conf
 #ldconfig
 
 (tail -f -n 0 $APP_ROOT/apache2/logs/*.log &)
-#exec  $APP_ROOT/apache2/bin/httpd -k start -f $APP_ROOT/apache2/conf/httpd.conf
+exec  $APP_ROOT/apache2/bin/httpd -k start -f $APP_ROOT/apache2/conf/httpd.conf
 #exec $APP_ROOT/apache2/bin/apachectl start
 # ------------------------------------------------------------------------------------------------
