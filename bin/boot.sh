5669476aeff18e2bc1b6f2d21dcb9b87ed0f438c
hmlingesh <Lingesh.M@happiestminds.com>
2015-01-08 23:15:10 +0530

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -2,6 +2,7 @@
 
 export APP_ROOT=$HOME
 
+# -------------------------------config file manipulation-----------------------------------------------------------------
 apache_conf_file=$APP_ROOT/apache2/conf/httpd.conf
 if [ -f $APP_ROOT/public/httpd.conf ]
 then
@@ -12,16 +13,16 @@ mv $apache_conf_file $APP_ROOT/apache2/conf/orig.conf
 erb $APP_ROOT/apache2/conf/orig.conf > $APP_ROOT/apache2/conf/httpd.conf
 
 # ------------------------------------------------------------------------------------------------
+
+# -------------------------------------------just for debugging purpose-----------------------------------------------------
 echo "----> list of files and directory:$(ls -l)" 
 
 for i in $(ls -d  $HOME/*/); do echo ${i%%/}; done
-
-#export PATH=$PATH:$APP_ROOT/apache2/lib
-#ldd $APP_ROOT/apache2/bin/httpd
 cat $APP_ROOT/apache2/conf/httpd.conf
-
-#ldconfig
 grep -i "Listen" $APP_ROOT/apache2/conf/httpd.conf
+# ------------------------------------------------------------------------------------------------
+
+
 #(tail -f -n 0 $APP_ROOT/apache2/logs/*.log &)
 exec  $APP_ROOT/apache2/bin/httpd -k start -f $APP_ROOT/apache2/conf/httpd.conf
 #exec $APP_ROOT/apache2/bin/apachectl start
