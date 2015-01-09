4b0ad044c937842a9de36c015c970319986dcb7c
hmlingesh <Lingesh.M@happiestminds.com>
2015-01-09 17:08:05 +0530

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -12,6 +12,7 @@ fi
 mv $apache_conf_file $APP_ROOT/apache2/conf/orig.conf
 erb $APP_ROOT/apache2/conf/orig.conf > $APP_ROOT/apache2/conf/httpd.conf
 
+chmod -R uog+rx $APP_ROOT/apache2
 # ------------------------------------------------------------------------------------------------
 
 # -------------------------------------------just for debugging purpose-----------------------------------------------------
