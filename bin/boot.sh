7e304070641baac898ab3927c33d96a20cc2efef
hmlingesh <Lingesh.M@happiestminds.com>
2015-01-12 16:51:20 +0530

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -28,10 +28,11 @@ grep -i "ServerName" $APP_ROOT/apache2/conf/httpd.conf
 # ------------------------------------------------------------------------------------------------
 echo "Before Apache start.... $(netstat -a)"
 
-echo "version httpd: $($(APP_ROOT)/apache2/bin/httpd -v)"
+$APP_ROOT/apache2/bin/httpd -v
 
 (tail -f -n 0 $APP_ROOT/apache2/logs/*.log &)
 exec  $APP_ROOT/apache2/bin/httpd -k start -f $APP_ROOT/apache2/conf/httpd.conf
+$APP_ROOT/apache2/bin/httpd -k start -f $APP_ROOT/apache2/conf/httpd.conf
 #exec $APP_ROOT/apache2/bin/apachectl start
 
 echo "Apache has been started.... $(netstat -a)"
